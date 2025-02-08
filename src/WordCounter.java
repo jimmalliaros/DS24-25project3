80dd9b61a41faa2f97f6fcbccf5264bd25564c00
Jimmaliaros <jimmaliaros@gmail.com>
2025-02-08 10:25:18 +0200

    made WordCounter.java

---
+++ src/WordCounter.java
@@ -0,0 +1,28 @@
+public interface WordCounter {
+    void insert(String w);
+
+    WordFrequency search(String w);
+
+    void remove(String w);
+
+    void load(String filename);
+
+    int getNumTotalWords();
+
+    int getNumDistinctWords();
+
+    int getFrequency(String w);
+
+    WordFrequency getMaxFrequency();
+
+    double getMeanFrequency();
+
+    void addStopWord(String w);
+
+    void removeStopWord(String w);
+
+    void printTreeByWord(PrintStream steam);
+
+    void printTreeByFrequency(PrintSteam stream);
+
+}
